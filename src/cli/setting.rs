f08c0afafa50919af46433febae886c5f34b7d6f
veselinoWiktor <vveselinov11@gmail.com>
2025-01-02 18:05:03 +0200

    Fix setting.rs with more concise description

--- src/cli/setting.rs
+++ src/cli/setting.rs
@@ -4,24 +4,34 @@ use structopt_derive::StructOpt;
 #[derive(StructOpt, Debug)]
 #[structopt(name = "MDC", about = "My Dummy Compiler Driver")]
 pub struct Settings {
+    #[structopt(parse(from_os_str))]
+    pub file_path: PathBuf,
+
     #[structopt(
         short = "l",
         long = "lex",
-        help = "Directs it to run the lexer, but stop before parsing"
+        help = "Directs it to run the tokenizer, but stop before parsing"
     )]
     pub lex: bool,
 
     #[structopt(
         short = "p",
         long = "parse",
-        help = "Directs it to run the lexer and parser, but stop before assembly generation"
+        help = "Directs it to run the tokenizer and parser, but stop before assembly generation"
     )]
     pub parse: bool,
 
     #[structopt(
+        short = "t",
+        long = "tacky",
+        help = "Directs it to run the tokenizer, parser and tacky generation, but stop before assembly generation"
+    )]
+    pub tacky: bool,
+
+    #[structopt(
         short = "c",
         long = "codegen",
-        help = "Directs it to perform lexing, parsing, and assembly generation, but stop before code emission"
+        help = "Directs it to perform tokenizer, parsing, tacky generation and assembly generation, but stop before code emission"
     )]
     pub codegen: bool,
 
@@ -31,14 +41,4 @@ pub struct Settings {
         help = "Directs it to emit only an assembly file, but not assemble or link it"
     )]
     pub emit_assembly: bool,
-
-    #[structopt(
-        short = "t",
-        long = "tacky",
-        help = "Directs it to emit only an assembly file, but not assemble or link it"
-    )]
-    pub tacky: bool,
-
-    #[structopt(parse(from_os_str))]
-    pub file_path: PathBuf,
 }
